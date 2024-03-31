f108cefa934437d703f168e6d13af5230dc725e0
Lev Alterman <leva.alm@gmail.com>
2024-03-31 19:24:38 +0300

    Change set_page_bg function

--- utils.py
+++ utils.py
@@ -6,24 +6,21 @@ import torch
 from torchvision import transforms
 
 
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
+def set_page_bg(image_file):
+    with open(image_file, "rb") as f:
+        img_data = f.read()
+    b64_encoded = base64.b64encode(img_data).decode()
+    style = f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{b64_encoded});
+            background-size: cover;
+        }}
+        </style>
+    """
+    st.markdown(style, unsafe_allow_html=True)
     return
+
 
 def predict(image, model, labels):
     transform = transforms.Compose([transforms.Resize((224,224)),
